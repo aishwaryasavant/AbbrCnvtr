f6675c1dde2d35d33c4abe4f15da08cc9b6a2dbd
aishwaryasavant <aishwaryasavant@gmail.com>
2015-05-06 00:32:03 -0400

    Abbrivation Converter

---
+++ twtAbbrAppl/src/main/java/com/twtappl/myapp/abbrCnvtr/AbbrConstants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.twtappl.myapp.abbrCnvtr;
+
+/**
+ * @author T
+ *
+ */
+public class AbbrConstants {
+	public final static String FILE_PATH = "C:/abbr-ws/twtAbbrAppl/src/main/resources";
+	public final static String FOLDER_NAME = FILE_PATH+"/data/file.csv";
+	public final static String ABR_LIST = FILE_PATH+"/data/abbrv.txt";
+}
